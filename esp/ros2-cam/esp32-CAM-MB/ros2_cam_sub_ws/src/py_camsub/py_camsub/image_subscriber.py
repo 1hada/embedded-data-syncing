3f23edc229c47f750a1d99b3114a2bf69ef37ae4
jay s <jonsanria@gmail.com>
2024-05-15 19:13:05 -0400

    adding source for topic

--- esp/ros2-cam/esp32-CAM-MB/ros2_cam_sub_ws/src/py_camsub/py_camsub/image_subscriber.py
+++ esp/ros2-cam/esp32-CAM-MB/ros2_cam_sub_ws/src/py_camsub/py_camsub/image_subscriber.py
@@ -7,7 +7,7 @@ from cv_bridge import CvBridge
 cd ros2_cam_sub_ws  # Navigate to your package directory
 colcon build --packages-select py_camsub    # Build the package
 source install/setup.bash  # Source the package setup script
-
+ros2 run py_camsub listener 
 """
 
 class ImageSubscriber(Node):
@@ -15,7 +15,7 @@ class ImageSubscriber(Node):
         super().__init__('image_subscriber')
         self.subscription = self.create_subscription(
             Image,
-            '/image/color/1',
+            '/image/color/source_1',
             self.image_callback,
             10
         )
@@ -26,8 +26,8 @@ class ImageSubscriber(Node):
         cv_image = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         # Process the image (e.g., display, save, or analyze)
         # Example: Display the image
-        cv2.imshow('Image', cv_image)
-        cv2.waitKey(1)
+        #cv2.imshow('Image', cv_image)
+        #cv2.waitKey(1)
 
 def main(args=None):
     rclpy.init(args=args)
